09aed8ac3607648a5809b1f40f6bafee3c26094c
Wenxin Zheng <peterzheng98@sjtu.edu.cn>
2020-10-21 17:02:46 +0800

    commit

--- parser.py
+++ parser.py
@@ -1,1 +1,11 @@
 import clang
+import sys
+import clang.cindex
+
+from clang.cindex import Config
+from clang.cindex import Cursor
+from clang.cindex import CursorKind
+
+Config.set_library_file("/usr/lib/llvm-10/lib/libclang-10.so.1")
+index = clang.cindex.Index.create()
+tu = index.parse('./ParsingSample/cf-96100658.c')
